39b26d40e9130ef9b7988191c5dd8ec9f10d3d26
pandeyprakhar00 <41755577+pandeyprakhar00@users.noreply.github.com>
2020-05-23 20:05:47 +0530

    Edit 1

--- Test.js
+++ Test.js
@@ -1,1 +1,12 @@
-
+Module.register("MMM-Test", {
+  defaults: {},
+  start: function () {},
+  getDom: function() {
+            var element = document.createElement("div")
+            element.className = "myContent"
+            element.innerHTML = "Hello, World!"
+            return element
+  },
+  notificationReceived: function() {},
+  socketNotificationReceived: function() {},
+})
